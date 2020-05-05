176cb3d351b02d656f38a1aaae587b0d2a2e959f
abaddon83 <abaddon83@gmail.com>
2020-05-05 01:53:24 +0100

    test fixed

--- src/test/scala/com/abaddon83/legal/domainModel/adapters/ddMandateAdapters/DDMandateRoutesTest.scala
+++ src/test/scala/com/abaddon83/legal/domainModel/adapters/ddMandateAdapters/DDMandateRoutesTest.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
 import akka.http.scaladsl.server._
 import Directives._
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.StatusCodes
 
 import scala.concurrent.duration._
 import akka.http.scaladsl.model.StatusCodes._
@@ -22,35 +23,29 @@ import org.scalatest.time.{Millis, Seconds, Span}
 
 
 class DDMandateRoutesTest extends WordSpec with Matchers with ScalatestRouteTest with Eventually {
-
-  val ddMandateRepository: DDMandateRepositoryPort = new FakeDDMandateRepositoryAdapter()
-  val bankAccountPort: BankAccountPort = new FakeBankAccountAdapter()
-  val creditorPort: CreditorPort = new FakeCreditorAdapter()
-  val ddMandateService: DDMandateService =   new DDMandateService(ddMandateRepository,bankAccountPort,creditorPort)
-  val contractRepository: ContractRepositoryPort = new FakeContractRepositoryAdapter();
-  val fileRepository: FileRepositoryPort = new FakeFileRepositoryAdapter()
-  val contractService: ContractService = new ContractService(contractRepository, fileRepository)
-
-  val ddMandateAdapter: DDMandateAdapter = new DDMandateAdapter(ddMandateService,contractService)
-
-  val ddMandateRoutes = DDMandateRoutes(ddMandateAdapter)
-
-  implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(3, Seconds)), interval = scaled(Span(5, Millis)))
-
-  "The service" should {
-
-    "return a greeting for GET requests to the root path" in {
-      // tests:
-
-      Get("/api/ddmandates") ~> ddMandateRoutes.route ~> check{
-        //eventually{
-          status shouldBe OK
-          responseAs[String] shouldEqual "Hello world"
-        //}
+    val ddMandateRepository: DDMandateRepositoryPort = new FakeDDMandateRepositoryAdapter()
+    val bankAccountPort: BankAccountPort = new FakeBankAccountAdapter()
+    val creditorPort: CreditorPort = new FakeCreditorAdapter()
+    val ddMandateService: DDMandateService =   new DDMandateService(ddMandateRepository,bankAccountPort,creditorPort)
+    val contractRepository: ContractRepositoryPort = new FakeContractRepositoryAdapter();
+    val fileRepository: FileRepositoryPort = new FakeFileRepositoryAdapter()
+    val contractService: ContractService = new ContractService(contractRepository, fileRepository)
+    val ddMandateAdapter: DDMandateAdapter = new DDMandateAdapter(ddMandateService,contractService)
+    val ddMandateRoutes = DDMandateRoutes(ddMandateAdapter)
 
 
+    //implicit override val patienceConfig = PatienceConfig(timeout = scaled(Span(3, Seconds)), interval = scaled(Span(5, Millis)))
+
+    "The service" should {
+
+      "return a greeting for GET requests to the root path" in {
+        // tests:
+        Get("/ddmandates") ~> ddMandateRoutes.route ~> check{
+          eventually{
+            status shouldBe OK
+            responseAs[String] shouldEqual "Hello world"
+          }
+        }
       }
     }
-  }
-
 }
